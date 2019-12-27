e3c5d6c938c3b48cae25340f2656147d33cc3322
eidng8 <cheung.jackey@gmail.com>
2019-12-27 12:42:21 +0800

    add random throttle

--- Objects/Interactive/Automated/Controllers/AccelerationWanderer.cs
+++ Objects/Interactive/Automated/Controllers/AccelerationWanderer.cs
@@ -43,27 +43,52 @@ namespace eidng8.SpaceFlight.Objects.Interactive.Automated.Controllers
                 return;
             }
 
+            this.RandomThrottle();
+            this.RandomWaypoint();
+
+            this._lastChoiceTime = Time.fixedTime;
+        }
+
+        /// <summary>Randomly pick a point far away from current position.</summary>
+        private void RandomWaypoint()
+        {
+            // This point should need 10s for it to get to with max speed.
             AccelerationMotorConfig mc = this.motorConfig;
             float range = Random.Range(mc.maxSpeed, mc.maxSpeed * 10);
-            Vector3 v = new Vector3(
+
+            // Randomly choose a direction of the waypoint
+            Vector3 dir = new Vector3(
                 Random.Range(-1f, 1f),
                 Random.Range(-1f, 1f),
                 Random.Range(-1f, 1f)
             );
 
-            var target = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            target.transform.localScale = Vector3.zero;
-            target.transform.position = v * range;
-            GameObject wp = this._waypoint;
+            // Pilot requires their targets to be Transform type.
+            // So we have to create a dummy object.
+            var dummy = GameObject.CreatePrimitive(PrimitiveType.Cube);
 
-            this.Pilot.Target = target.transform;
-            this._waypoint = target;
+            // Just to be sure the object won't be visible.
+            dummy.transform.localScale = Vector3.zero;
+
+            // Set the position using previously chosen direction and range.
+            dummy.transform.position = dir * range;
+
+            // Save current waypoint before setting a new one.
+            GameObject wp = this._waypoint;
+            this.Pilot.Target = dummy.transform;
+            this._waypoint = dummy;
+
+            // Destroy previous waypoint if there's one.
             if (this._shouldDestroyWaypoint) {
                 Object.DestroyImmediate(wp);
             }
 
-            this._lastChoiceTime = Time.fixedTime;
             this._shouldDestroyWaypoint = true;
+        }
+
+        private void RandomThrottle()
+        {
+            this.Throttle = Random.value;
         }
     }
 }
